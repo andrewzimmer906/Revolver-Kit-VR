2257f0b603de434454ddee421d20c61ea1e99dcb
Andrew Zimmer <andrewzimmer906@gmail.com>
2017-10-22 17:48:49 -0600

    First Commit

---
+++ Scripts/SVControllerInput.cs
@@ -0,0 +1,212 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public enum SVInputButton {
+	SVButton_None = -1,
+	SVButton_System = Valve.VR.EVRButtonId.k_EButton_System,
+	SVButton_Menu = Valve.VR.EVRButtonId.k_EButton_ApplicationMenu,
+	SVButton_Grip = Valve.VR.EVRButtonId.k_EButton_Grip,
+	SVButton_DPad_Left = Valve.VR.EVRButtonId.k_EButton_DPad_Left,
+	SVButton_DPad_Right = Valve.VR.EVRButtonId.k_EButton_DPad_Right,
+	SVButton_DPad_Down = Valve.VR.EVRButtonId.k_EButton_DPad_Down,
+	SVButton_DPad_Up = Valve.VR.EVRButtonId.k_EButton_DPad_Up,
+	SVButton_A = Valve.VR.EVRButtonId.k_EButton_A,
+	SVButton_Touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad,
+	SVButton_Trigger = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger
+};
+	
+
+public class SVControllerInput : MonoBehaviour {
+
+	//------------------------
+	// Variables
+	//------------------------
+	[Space(15)]
+	[Header("Grip Settings")]
+	public SVInputButton gripButton = SVInputButton.SVButton_Grip;
+	public SVInputButton releaseGripButton = SVInputButton.SVButton_None;
+	public bool gripAutoHolds = false;
+
+	[Space(15)]
+	[Header("Firing Settings")]
+	public SVInputButton triggerButton = SVInputButton.SVButton_Trigger;
+
+	[Space(15)]
+	[Header("Reload Settings")]
+	public SVInputButton openBarrelButton = SVInputButton.SVButton_None;
+	public SVInputButton closeBarrelButton = SVInputButton.SVButton_None;
+
+	public bool openWithPhysics = true;
+	public float openAcceleration = 4f;
+	public float openEmptyAcceleration = 2f;
+	public float closeAcceleration = -2f;
+
+	//------------------------
+	// Private Variables
+	//------------------------
+	[HideInInspector]
+	public GameObject activeController;
+	[HideInInspector]
+	public SteamVR_Controller.Device activeControllerDevice;
+	[HideInInspector]
+	public SteamVR_RenderModel activeRenderModel;
+	[HideInInspector]
+	public bool activeControllerIsRight = false;
+	[HideInInspector]
+	public bool hasActiveController = false;
+
+	private float rumbleStrength = 1f;
+
+
+
+	private SteamVR_ControllerManager controllerManager;
+
+	//------------------------
+	// Setup
+	//------------------------
+	void Start() {
+		controllerManager = Object.FindObjectOfType<SteamVR_ControllerManager> ();
+		Assert.IsNotNull (controllerManager, "SVControllerInput Needs a SteamVR_ControllerManager in the scene to function correctly.");
+	}
+
+	//------------------------
+	// Getters
+	//------------------------
+	private SteamVR_Controller.Device Controller(SteamVR_TrackedObject trackedObject) {
+		return SteamVR_Controller.Input((int)trackedObject.index);
+	}
+
+	public GameObject LeftHandController {
+		get {
+			if (controllerManager.left != null &&
+			    controllerManager.left.activeInHierarchy) {
+				return controllerManager.left;
+			}
+			return null;
+		}
+	}
+
+	public GameObject RightHandController {
+		get {
+			if (controllerManager.right != null &&
+				controllerManager.right.activeInHierarchy) {
+				return controllerManager.right;
+			}
+			return null;
+		}
+	}
+
+	//------------------------
+	// Input Checkers
+	//------------------------
+
+	public bool GetGripButtonDown(GameObject controller) {
+		return this.GetButtonDown (controller, this.gripButton);
+	}
+
+	public bool GetGripButtonPressed(GameObject controller) {
+		return this.GetButtonPressDown (controller, this.gripButton);
+	}
+
+	public bool GetReleaseGripButtonPressed(GameObject controller) {
+		return this.GetButtonPressDown (controller, this.releaseGripButton);
+	}
+
+	public bool GetTriggerButtonPressed(GameObject controller) {
+		return this.GetButtonPressDown (controller, this.triggerButton);
+	}
+
+	public bool GetOpenBarrelPressed(GameObject controller) {
+		return this.GetButtonPressDown (controller, this.openBarrelButton);
+	}
+
+	public bool GetCloseBarrelPressed(GameObject controller) {
+		return this.GetButtonPressDown (controller, this.closeBarrelButton);
+	}
+
+	//------------------------
+	// Public
+	//------------------------
+
+	public bool GetButtonDown(GameObject controller, SVInputButton button) {
+		if (button == SVInputButton.SVButton_None)
+			return false;
+		return Controller (controller.GetComponent<SteamVR_TrackedObject>()).GetPress((Valve.VR.EVRButtonId)button); //TODO : Cache this component for performance
+	}
+
+	public bool GetButtonPressDown(GameObject controller, SVInputButton button) {
+		if (button == SVInputButton.SVButton_None)
+			return false;
+		return Controller (controller.GetComponent<SteamVR_TrackedObject>()).GetPressDown((Valve.VR.EVRButtonId)button); //TODO : Cache this component for performance
+	}
+
+	public bool SetActiveController(GameObject activeController) {
+		if (activeController == SVControllerManager.activeLeftController ||
+		    activeController == SVControllerManager.activeRightController) {
+			return false;
+		}
+
+		this.activeController = activeController;
+		this.activeControllerDevice = Controller (activeController.GetComponent<SteamVR_TrackedObject>());  //TODO : Cache this component for performance
+		this.activeRenderModel = this.activeController.GetComponentInChildren<SteamVR_RenderModel>();
+		this.activeControllerIsRight = (controllerManager.right == activeController);
+
+		if (this.activeControllerIsRight) {
+			SVControllerManager.activeRightController = this.activeController;
+		} else {
+			SVControllerManager.activeLeftController = this.activeController;
+		}
+
+		hasActiveController = true;
+
+		return true;
+	}
+
+	public void ClearActiveController() {
+		hasActiveController = false;
+
+		this.activeController = null;
+		this.activeControllerDevice = null;
+		this.activeRenderModel = null;
+
+		if (this.activeControllerIsRight) {
+			SVControllerManager.activeRightController = null;
+		} else {
+			SVControllerManager.activeLeftController = null;
+		}
+	}
+
+	public void RumbleActiveController(float rumbleLength) {
+		if (activeControllerDevice != null) {
+			StartCoroutine( LongVibration(activeControllerDevice, rumbleLength, rumbleStrength) );
+		}
+	}
+
+	//------------------------
+	// Haptics
+	//------------------------
+
+	//length is how long the vibration should go for
+	//strength is vibration strength from 0-1
+	private IEnumerator LongVibration(SteamVR_Controller.Device device, float totalLength, float strength) {
+		ushort rLength = (ushort)Mathf.Lerp (0, 3999, strength);
+		for (float i = 0f; i < totalLength; i += Time.deltaTime) {
+			device.TriggerHapticPulse(rLength);
+			yield return null;
+		}
+	}
+
+	//vibrationCount is how many vibrations
+	//vibrationLength is how long each vibration should go for
+	//gapLength is how long to wait between vibrations
+	//strength is vibration strength from 0-1
+	IEnumerator LongVibration(SteamVR_Controller.Device device, int vibrationCount, float vibrationLength, float gapLength, float strength) {
+		strength = Mathf.Clamp01(strength);
+		for(int i = 0; i < vibrationCount; i++) {
+			if(i != 0) yield return new WaitForSeconds(gapLength);
+			yield return StartCoroutine(LongVibration(device, vibrationLength, strength));
+		}
+	}
+}
---
+++ Scripts/SVGrabbable.cs
@@ -0,0 +1,201 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+
+public class SVGrabbable : MonoBehaviour {
+
+    //------------------------
+    // Variables
+    //------------------------
+    public float grabDistance = 1;
+	public float grabFlyTime = 2f;
+	public bool shouldFly = true;
+
+	[HideInInspector]
+	public bool inHand = false;
+
+    private SVOutline outlineComponent;
+
+	private SVControllerInput input;
+	private float grabStartTime;
+	private Vector3 grabStartPosition;
+
+	private float kickStartTime;
+	private float maxKickDuration = 1f;
+	private float minKickAngle = .25f;
+	private bool isKicking = false;
+	private Quaternion kickOffset;
+
+	private Quaternion grabStartRotation;
+		
+    //------------------------
+    // Init
+    //------------------------
+    void Start () {
+        outlineComponent = this.gameObject.GetComponent<SVOutline>();
+		this.input = this.gameObject.GetComponent<SVControllerInput> ();
+    }
+
+    //------------------------
+    // Update
+    //------------------------
+    void Update() {
+		if (!this.input.hasActiveController) {
+            this.UngrabbedUpdate();
+        } else {
+            this.GrabbedUpdate();
+        }
+    }
+
+    private void UngrabbedUpdate() {
+		this.inHand = false;
+
+        float distanceToLeftHand = 1000;
+		if (input.LeftHandController != null) {
+			distanceToLeftHand = (this.transform.position - input.LeftHandController.transform.position).magnitude;
+        }
+
+		float distanceToRightHand = 1000;
+		if (input.RightHandController != null) {
+			distanceToRightHand = (this.transform.position - input.RightHandController.transform.position).magnitude;
+		}
+
+        if (grabDistance > distanceToLeftHand ||
+            grabDistance > distanceToRightHand) {
+            float distance = Mathf.Min(distanceToLeftHand, distanceToRightHand);
+            if (this.outlineComponent) {
+                float distanceForHighlight = grabDistance / 4f;
+                float highlight = Mathf.Max(0, Mathf.Min(1, (grabDistance - distance) / distanceForHighlight));
+                outlineComponent.outlineActive = highlight;
+            }
+
+			// order them based on distance
+			GameObject firstController = null;
+			GameObject secondController = null;
+
+			if (distanceToLeftHand < distanceToRightHand) {
+				if (SVControllerManager.nearestGrabbableToLeftController == this)
+					firstController = input.LeftHandController;
+				
+				if (SVControllerManager.nearestGrabbableToRightController == this)
+					secondController = input.RightHandController;	
+			} else {
+				if (SVControllerManager.nearestGrabbableToRightController == this)
+					firstController = input.RightHandController;
+
+				if (SVControllerManager.nearestGrabbableToLeftController == this)
+					secondController = input.LeftHandController;
+			}
+
+			TrySetActiveController (firstController);
+			TrySetActiveController (secondController);
+
+			// Update grabbable distance so we always grab the nearest revolver
+			if (distanceToLeftHand < SVControllerManager.distanceToLeftController || 
+				SVControllerManager.nearestGrabbableToLeftController == null ||
+				SVControllerManager.nearestGrabbableToLeftController == this) {
+				SVControllerManager.nearestGrabbableToLeftController = this;
+				SVControllerManager.distanceToLeftController = distanceToLeftHand;
+			}
+
+			if (distanceToRightHand < SVControllerManager.distanceToRightController || 
+				SVControllerManager.nearestGrabbableToRightController == null ||
+				SVControllerManager.nearestGrabbableToRightController == this) {
+				SVControllerManager.nearestGrabbableToRightController = this;
+				SVControllerManager.distanceToRightController = distanceToRightHand;
+			}
+				
+        } else {
+            outlineComponent.outlineActive = 0;
+        }
+    }
+
+    private void GrabbedUpdate() {
+		if (input.gripAutoHolds) {
+			if (input.GetReleaseGripButtonPressed (input.activeController)) {
+				this.ClearActiveController ();
+				return;
+			}
+		} else if (!input.GetGripButtonDown(input.activeController)) {
+			this.ClearActiveController ();
+			return;
+        }
+
+		float percComplete = (Time.time - this.grabStartTime) / this.grabFlyTime;
+		if (percComplete < 1 && this.shouldFly) {
+			this.inHand = false;
+			transform.position = Vector3.Lerp (this.grabStartPosition, this.input.activeController.transform.position, percComplete);
+			transform.rotation = Quaternion.Lerp (this.grabStartRotation, this.input.activeController.transform.rotation, percComplete);
+		} else if (isKicking) {
+			this.kickOffset = Quaternion.Lerp (this.kickOffset, Quaternion.identity, 0.05f);
+			this.transform.SetPositionAndRotation (this.input.activeController.transform.position, this.input.activeController.transform.rotation * this.kickOffset);
+
+			float curAngle = Quaternion.Angle (this.kickOffset, Quaternion.identity);
+			if (curAngle < minKickAngle || Time.time - this.kickStartTime > maxKickDuration) {
+				this.isKicking = false;
+			}
+		} else {
+			this.inHand = true;
+			this.transform.SetPositionAndRotation(this.input.activeController.transform.position, this.input.activeController.transform.rotation);
+		}
+    }
+
+	//------------------------
+	// Kick
+	//------------------------
+	public void EditGripForKick(float kickForce) {
+		kickStartTime = Time.time;
+		isKicking = true;
+
+		Quaternion upRotation = Quaternion.AngleAxis (-90, Vector3.right);
+		this.kickOffset = Quaternion.RotateTowards (Quaternion.identity, upRotation, kickForce);
+	}
+
+    //------------------------
+    // State Changes
+    //------------------------
+	private void TrySetActiveController(GameObject controller) {
+		if (this.input.hasActiveController)
+			return;	
+
+		if (controller == null)
+			return;
+
+		if (input.gripAutoHolds) {
+			if (!input.GetGripButtonPressed(controller)) {
+				return;
+			}
+		} else {
+			if (!input.GetGripButtonDown (controller)) {
+				return;
+			}
+		}
+
+		if (this.input.SetActiveController (controller)) {
+			this.grabStartTime = Time.time;
+			this.grabStartPosition = this.gameObject.transform.position;
+			this.grabStartRotation = this.gameObject.transform.rotation;
+			outlineComponent.outlineActive = 0;
+		
+			Rigidbody rigidbody = this.GetComponent<Rigidbody> ();
+			rigidbody.isKinematic = true;
+
+			// hide the controller model
+			this.input.activeRenderModel.gameObject.SetActive (false);
+		}
+    }
+
+	private void ClearActiveController() {
+		Rigidbody rigidbody = this.GetComponent<Rigidbody> ();
+		rigidbody.isKinematic = false;
+		rigidbody.velocity = this.input.activeControllerDevice.velocity;
+		rigidbody.angularVelocity =  this.input.activeControllerDevice.angularVelocity;
+
+		// Show the render model
+		this.input.activeRenderModel.gameObject.SetActive (true);
+
+		this.input.ClearActiveController ();
+	}
+}
